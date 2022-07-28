e88cd1da1f859770ee83f9f8a06c2f20a5c5dd8a
wagnersistemalima <wagner.sistemalima@gmail.com>
2022-07-28 17:44:01 -0300

    projeto contendo aplicacao back end construida no curso da alura

---
+++ src/main/kotlin/br/com/alura/forum/service/CursoService.kt
@@ -0,0 +1,15 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.repository.CursoRepository
+import org.springframework.stereotype.Service
+
+@Service
+class CursoService(private val repository: CursoRepository) {
+
+    fun buscarPorId(id: Long): Curso {
+        return repository.getOne(id)
+    }
+
+
+}
---
+++ src/test/kotlin/br/com/alura/forum/ForumApplicationTests.kt
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+class ForumApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
